9821909d96b2411a67399cfcf21e8014e6714064
solomen <solomonshaju123@gmail.com>
2022-06-07 10:05:14 +0530

    removed round button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,6 +16,7 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button bconvert,bselect;
+    Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b0,bback;
     ConstraintLayout mCLayout;
     String fromUnit = "Celcius";
     String toUnit = "Farenheit";
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             "Rankine",
             "Newton", "Delisle"
     };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +42,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         txtFrom = (EditText) findViewById(R.id.tfrom);
         txtTo = (EditText) findViewById(R.id.tto);
         bconvert.setOnClickListener(this);
+        b1 = (Button) findViewById(R.id.b1);
+        b1.setOnClickListener(this);
+        b2 = (Button) findViewById(R.id.b2);
+        b2.setOnClickListener(this);
+        b3 = (Button) findViewById(R.id.b3);
+        b3.setOnClickListener(this);
+        b4 = (Button) findViewById(R.id.b4);
+        b4.setOnClickListener(this);
+        b5 = (Button) findViewById(R.id.b5);
+        b5.setOnClickListener(this);
+        b6 = (Button) findViewById(R.id.b6);
+        b6.setOnClickListener(this);
+        b7 = (Button) findViewById(R.id.b7);
+        b7.setOnClickListener(this);
+        b8 = (Button) findViewById(R.id.b8);
+        b8.setOnClickListener(this);
+        b9 = (Button) findViewById(R.id.b9);
+        b9.setOnClickListener(this);
     }
     @Override
     public void onClick(View v){
