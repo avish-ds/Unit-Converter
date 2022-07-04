39eaa7246abd5cba43d72abbc94d78d5f9041af2
avish-ds <avishshetty222@gmail.com>
2022-07-04 19:53:29 +0530

    Complete app

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button b1,b2,b3;
+    Button b1,b2,b3,b4,b5,b6,b7;
 
     ConstraintLayout mCLayout;
 
@@ -31,6 +31,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         b3=findViewById(R.id.b3);
         b3.setOnClickListener(this);
 
+        b4=findViewById(R.id.b4);
+        b4.setOnClickListener(this);
+
+        b5=findViewById(R.id.b5);
+        b5.setOnClickListener(this);
+
+        b6=findViewById(R.id.b6);
+        b6.setOnClickListener(this);
+
+        b7=findViewById(R.id.b7);
+        b7.setOnClickListener(this);
+
     }
     @Override
     public void onClick(View v) {
@@ -45,6 +57,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         else if(v.equals(b3)){
             Intent intent = new Intent(this, MainActivityKG.class);
+            startActivity(intent);
+        }
+        else if(v.equals(b4)){
+            Intent intent = new Intent(this, MainActivityCGPA.class);
+            startActivity(intent);
+        }
+        else if(v.equals(b5)){
+            Intent intent = new Intent(this, MainActivityDECIMAL.class);
+            startActivity(intent);
+        }
+        else if(v.equals(b6)){
+            Intent intent = new Intent(this, MainActivityHEX.class);
+            startActivity(intent);
+        }
+
+        else if(v.equals(b7)){
+            Intent intent = new Intent(this, MainActivityPercent.class);
             startActivity(intent);
         }
 
